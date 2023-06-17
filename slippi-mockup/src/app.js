3b3fbbc2f52735656fb56da0593db3c096dea99c
andresdlRoca <andresdelaroca11@gmail.com>
2023-06-17 14:59:29 -0600

    Working on routes (Not functional yet)

--- slippi-mockup/src/app.js
+++ slippi-mockup/src/app.js
@@ -1,1 +1,24 @@
-var app = angular.module('Slippi', ['ngRoute']);
+var app = angular.module("Slippi-Homepage", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "views/home.html",
+            controller: "HomeController"
+        })
+        .when("/about", {
+            templateUrl: "views/about.html",
+            controller: "AboutController"
+        })
+        .when("/downloads", {
+            templateUrl: "views/downloads.html",
+            controller: "DownloadsController"
+        })
+        .when("/leaderboards", {
+            templateUrl: "views/leaderboards.html",
+            controller: "LeaderboardsController"
+        })
+        .otherwise({
+            redirectTo: "/about"
+        });
+});
+
